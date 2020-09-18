917dba05d24706b1f9f502e90a08ad54d36f8055
Marwan-Ezzat-Mohamed <Marwan-Ezzat-Mohamed@users.noreply.github.com>
2020-09-18 22:42:54 +0200

    ignore

--- menu.js
+++ menu.js
@@ -1,10 +1,9 @@
 // Tabbed Menu
 function openMenu(eventt, menuName) {
-    let i, x;
-    x = document.getElementsByClassName("menu");
-    for (i = 0; i < x.length; i++) {
-       x[i].style.display = "none";
-    }
-    document.getElementById(menuName).style.display = "block";
+  let i, x;
+  x = document.getElementsByClassName('menu');
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = 'none';
   }
-  
+  document.getElementById(menuName).style.display = 'block';
+}
