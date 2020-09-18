9ce2a7d5473762f2d1c73d78e188d856d5181375
Marwan-Ezzat-Mohamed <Marwan-Ezzat-Mohamed@users.noreply.github.com>
2020-09-18 22:41:04 +0200

    fixed totalprice not working bug

--- cart.js
+++ cart.js
@@ -5,43 +5,58 @@ let countOfitem = [];
 let cart;
 
 function OpenTheCart() {
-    quantity = JSON.parse(localStorage.getItem("quantityArray"));
+  quantity = JSON.parse(localStorage.getItem('quantityArray'));
 
-    let key = "";
-    let list = "<tr><th>Order</th><th>Price</th><th>Quantity</th></tr>\n";
-    let i = 0;
-    for (i = 0; i < localStorage.length; i++) {
-        key = localStorage.key(i);
-        if (key == "quantityArray") continue;
-        console.log(localStorage.getItem(key),quantity[localStorage.getItem(key)])
-        list += "<tr><td>" + key + "</td>\n<td>"
-            + localStorage.getItem(key) + "</td>\n<td>" + quantity[localStorage.getItem(key)] + "</td></tr>\n";
-    }
-    if (list == "<tr><th>Order</th><th>Price</th></tr>\n") {
-        list += "<tr><td><i>empty</i></td>\n<td><i>empty</i></td></tr>\n";
-    }
-    document.getElementById('list').innerHTML = list;
+  let key = '';
+  let list = '<tr><th>Order</th><th>Price</th><th>Quantity</th></tr>\n';
+  let i = 0;
+  for (i = 0; i < localStorage.length; i++) {
+    key = localStorage.key(i);
+    if (
+      key == 'quantityArray' ||
+      key == 'cUser' ||
+      key == 'members' ||
+      key == 'totalprice'
+    )
+      continue;
+    console.log(localStorage.getItem(key), quantity[localStorage.getItem(key)]);
+    list +=
+      '<tr><td>' +
+      key +
+      '</td>\n<td>' +
+      localStorage.getItem(key) +
+      '</td>\n<td>' +
+      quantity[localStorage.getItem(key)] +
+      '</td></tr>\n';
+  }
+  if (list == '<tr><th>Order</th><th>Price</th></tr>\n') {
+    list += '<tr><td><i>empty</i></td>\n<td><i>empty</i></td></tr>\n';
+  }
+  document.getElementById('list').innerHTML = list;
 }
 function addtocart(TypeOfFood, price) {
-    if (localStorage.getItem(TypeOfFood) == null) {
-        localStorage.setItem(TypeOfFood, price);
-        Totalprice += price;
-        quantity[price] = 1;
-        countOfitem[price] = 1;
-    }
-    else if (localStorage.getItem(TypeOfFood) != null) {
-        countOfitem[price]++;
-        Totalprice += price;
-        quantity[price] = countOfitem[price];
-    }
-    let quantity_sterialized = JSON.stringify(quantity);
-    localStorage.setItem("quantityArray", quantity_sterialized);
+  Totalprice = JSON.parse(localStorage.getItem('totalprice'));
+  if (localStorage.getItem(TypeOfFood) == null) {
+    localStorage.setItem(TypeOfFood, price);
+    Totalprice += price;
+    quantity[price] = 1;
+    countOfitem[price] = 1;
+  } else if (localStorage.getItem(TypeOfFood) != null) {
+    countOfitem[price]++;
+    Totalprice += price;
+    quantity[price] = countOfitem[price];
+  }
+  let quantity_sterialized = JSON.stringify(quantity);
+  let totalPrice_sterialized = JSON.stringify(Totalprice);
+  localStorage.setItem('quantityArray', quantity_sterialized);
+  localStorage.setItem('totalprice', totalPrice_sterialized);
 }
 function ClearAll() {
-    Totalprice = 0;
-    localStorage.clear();
-    document.getElementById('list').innerHTML = Totalprice;
+  Totalprice = 0;
+  localStorage.clear();
+  document.getElementById('list').innerHTML = Totalprice;
 }
 function TotalPrice() {
-    document.getElementById('list').innerHTML = Totalprice;
+  Totalprice = JSON.parse(localStorage.getItem('totalprice'));
+  document.getElementById('list').innerHTML = Totalprice;
 }
